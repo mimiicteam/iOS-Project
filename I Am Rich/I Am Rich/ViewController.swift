542dce1f9051c4b12a2092b516114936d086693c
NGUYEN MINH DUC <nguyenminhduc@MacBook-Pro-cua-DUC.local>
2022-01-02 02:49:48 +0700

    iOS Project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by NGUYEN MINH DUC on 29/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
